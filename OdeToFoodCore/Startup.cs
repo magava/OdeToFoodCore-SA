61d50260dffe7f395f81e42b3f4dd03d7a1162a0
magava <magaava@gmail.com>
2018-05-24 00:42:32 +0400

    Replaced the hard coded greeting string with a greeting read from the added appsettings.json configuration source.

--- OdeToFoodCore/Startup.cs
+++ OdeToFoodCore/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OdeToFoodCore
@@ -18,7 +19,9 @@ namespace OdeToFoodCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, 
+                              IHostingEnvironment env,
+                              IConfiguration configuration)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +30,8 @@ namespace OdeToFoodCore
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World from OdeToFood!");
+                var greeting = configuration["Greeting"];
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
