102d52ca9cfea297dec6e1627a87bc8a9e18726c
magava <magaava@gmail.com>
2019-01-09 03:29:16 +0400

    3.3.3. Added a middleware using app.Use method.

--- OdeToFoodCore/Startup.cs
+++ OdeToFoodCore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFoodCore
 {
@@ -22,12 +23,34 @@ namespace OdeToFoodCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, 
                               IHostingEnvironment env,
-                              IGreeter greeter)
+                              IGreeter greeter, 
+                              ILogger<Startup> logger)
         {
             //if (env.IsDevelopment())
             //{
             //    app.UseDeveloperExceptionPage();
             //}
+
+
+
+            app.Use(next => {
+                return async context =>
+                {
+                    logger.LogInformation("Request incoming...");
+                    if (context.Request.Path.StartsWithSegments("/mym"))
+                    {
+                        await context.Response.WriteAsync("Hit!!!");
+                        logger.LogInformation("Request handled...");
+                    }
+                    else
+                    {
+                        await next(context);
+                        logger.LogInformation("Response outgoing...");
+                    }
+                };
+              
+            });
+
 
 
             /**
