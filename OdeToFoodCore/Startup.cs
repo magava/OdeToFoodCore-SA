2705313d0bc6b5f8bc9f3382f0f0800abe87c49d
magava <magaava@gmail.com>
2019-01-12 21:23:23 +0400

    3.6.3. Added UseDefaultFiles to get the content of index.html when we go to localhost:<portNum> (in our case localhost:52024).

--- OdeToFoodCore/Startup.cs
+++ OdeToFoodCore/Startup.cs
@@ -29,6 +29,20 @@ namespace OdeToFoodCore
                 app.UseDeveloperExceptionPage();
             }
 
+
+            /**
+             * DefaultFiles will look at the incoming request. 
+             * If that request is for a directory like the root of the application
+             * this middleware will look inside of that directory to see if there is
+             * a default file. The name of the default file is configurable.
+             * One of the default of the default filenames is index.html.  
+             * UseDefaultFiles won't serve up the default file, it won't read that file
+             * from file system and send it back to the user.
+             * UseDefaultFiles will change the request path and then invoke the next middleware,
+             * which is UseStaticFiles, which will serve up the content.
+             */
+            app.UseDefaultFiles();
+
             app.UseStaticFiles();
            
             app.Run(async (context) =>
