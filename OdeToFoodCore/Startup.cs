c59cfe0493c5b6d5d11be4c3c6d667ee665f18fc
magava <magaava@gmail.com>
2019-01-12 16:50:38 +0400

    3.5.1. Added a Production profile to the launchSettings.json

--- OdeToFoodCore/Startup.cs
+++ OdeToFoodCore/Startup.cs
@@ -28,11 +28,15 @@ namespace OdeToFoodCore
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler();
+            }
 
             app.Run(async (context) =>
             {
                 var greeting = greeter.GetMessageOfTheDay();
-                await context.Response.WriteAsync(greeting);
+                await context.Response.WriteAsync($"{greeting} : {env.EnvironmentName}");
             });
         }
     }
