687f8fb82e1c3a887d137bccb2c76ccbf328f989
magava <magaava@gmail.com>
2019-01-22 01:50:37 +0400

    3.7.1.1. Added services.AddMvc() inside the call to ConfigureServices to solve the problem with the InvalidOperationException that occured after the addition of app.UseMvcWithDefaultRoute()

--- OdeToFoodCore/Startup.cs
+++ OdeToFoodCore/Startup.cs
@@ -17,6 +17,7 @@ namespace OdeToFoodCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
