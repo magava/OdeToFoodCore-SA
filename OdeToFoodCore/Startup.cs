641457ed218b640fd9b27bb24a9b2a170ab1da2c
magava <magaava@gmail.com>
2019-01-22 01:01:02 +0400

    3.7.1 Setting Up MVC Middleware

--- OdeToFoodCore/Startup.cs
+++ OdeToFoodCore/Startup.cs
@@ -29,26 +29,11 @@ namespace OdeToFoodCore
                 app.UseDeveloperExceptionPage();
             }
 
+          
+            app.UseStaticFiles();
 
-            /**
-             * DefaultFiles will look at the incoming request. 
-             * If that request is for a directory like the root of the application
-             * this middleware will look inside of that directory to see if there is
-             * a default file. The name of the default file is configurable.
-             * One of the default of the default filenames is index.html.  
-             * UseDefaultFiles won't serve up the default file, it won't read that file
-             * from file system and send it back to the user.
-             * UseDefaultFiles will change the request path and then invoke the next middleware,
-             * which is UseStaticFiles, which will serve up the content.
-             */
-            //app.UseDefaultFiles();
-
-            //app.UseStaticFiles();
-
-
-            // UseFileServer installs the DefaultFiles and StaticFiles middlewares
-            app.UseFileServer();
-           
+            app.UseMvcWithDefaultRoute();
+       
             app.Run(async (context) =>
             {
                 var greeting = greeter.GetMessageOfTheDay();
