0e77adcdb8f6f1e6f6ece2deef2c03d02a05fe09
magava <magaava@gmail.com>
2019-01-22 02:20:43 +0400

    3_7_2_2. Added a Controllers folder and a HomeController class.

---
+++ OdeToFoodCore/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFoodCore.Controllers
+{
+
+    /**
+     * This is a usual C# class.
+     * By default HomeController will receive a request to the root of the application.
+     * MVC will create the HomeController class and invoke the Index method.
+     * We'll see "Hello from HomeController!!!" when go to the root.
+    */
+
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Hello from HomeController!!!";
+        }
+    }
+}
