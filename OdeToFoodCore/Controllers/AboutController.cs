33efdb0e7bcb9c476bead7787067e3c01339d2ab
magava <magaava@gmail.com>
2019-06-08 02:43:17 +0400

    dev_4_5_0_Added route attributes to AboutController.

--- OdeToFoodCore/Controllers/AboutController.cs
+++ OdeToFoodCore/Controllers/AboutController.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFoodCore.Controllers
 {
+    // We want the /about to reach the Phone action
+    // We want the Phone action to be the default action for this controller
+
+
+
+    // MVC framework will go to AboutController but as there are two public methods
+    // it doesn't know which one to execute when we go to /about and we'll get
+    // AmbiguousActionException: Multiple actions matched. 
+    // The following actions matched route data and had all constraints satisfied:
+    // OdeToFoodCore.Controllers.AboutController.Phone(OdeToFoodCore)
+    // OdeToFoodCore.Controllers.AboutController.Address(OdeToFoodCore)
+
+
+    [Route("about")]
     public class AboutController
     {
-
+        [Route("")]
         public string Phone()
         {
             return "777 777 7777";
